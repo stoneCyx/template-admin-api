7d4f08e40f18861891b85b4788ebfce3144b8b40
魏文锋 <hsxgcsw@163.com>
2021-10-12 11:03:11 +0800

    fix:登录

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post,HttpStatus, HttpException } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('/api/v1')
@@ -8,6 +8,20 @@ export class AppController {
   @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Post('login')
+  async login(@Body() accountInfo) {
+    interface retType {
+      username:String;
+      role:String
+    }
+    const { username, password } = accountInfo;
+    const ret = await this.appService.login({ username, password });
+    if(!ret){
+      return { message:'用户不存在',code:404 }
+    }
+
+    return ret;
   }
 
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+const crypto = require('crypto');
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  login({username, password}){
+    return  new Promise((resolve) => setTimeout(()=>{
+      if(username === 'admin' && password === '123456'){
+       
+        const md5 = crypto.createHash('md5');
+        const accesstoken = md5.update(JSON.stringify({username:'admin',role:'admin'})).digest('base64');
+         resolve({
+          username:'admin',
+          role:'admin',
+          accesstoken
+        })
+      }else{
+        resolve(null);
+      }
+    }, 500));
   }
 }
